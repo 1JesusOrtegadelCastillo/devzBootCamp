641fc343ce2c358d87453c82e9d1fd1d0d4639ec
JesusOrtega <jesusortegadc@gmail.com>
2021-04-17 15:21:40 -0700

    Palindrome exercise modified, added two pointers

--- Main.java
+++ Main.java
@@ -1,30 +1,42 @@
 package com.company;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String reverseSentence = "";
+
         Scanner sc = new Scanner(System.in);
 
-        // Saving sentence into a variable
-        System.out.println("Write your sentence:");
-        String sentence = sc.nextLine();
-        // string to lowercase and removing accents, blanks and special characters
-        sentence = sentence.toLowerCase().replace("á", "a").replace("é", "e")
-                  .replace("í", "i").replace("ó", "o").replace("ú", "u")
-                  .replace(" ", "").replaceAll("[^\\dA-Za-z]", "");
-        // Reversing given sentence
-        for (int i = sentence.length()-1; i>=0; i--){
-            reverseSentence = reverseSentence + sentence.charAt(i);
-        }
-        if (sentence.equals(reverseSentence)){
-            System.out.println("true, " + sentence + " is a palindrome");
-        } else {
-            System.out.println("false, " + sentence + " is not a palindrome");
-        }
+        System.out.println("Write a sentence:");
+        String phrase = sc.nextLine();
+
+        // Cleaning String
+         phrase = phrase.toLowerCase()
+                .replace("á", "a")
+                .replace("é", "e")
+                .replace("í", "i")
+                .replace("ó", "o")
+                .replace("ú", "u")
+                .replace(" ", "")
+                .replace("[a-zA-Z0-9]", "");
+
+         int x = 0,
+             y = phrase.length() - 1;
+
+         while(x < y){
+             if (phrase.charAt(x) != phrase.charAt(y)){
+                 System.out.println("false");
+                 System.exit(0);
+             }
+             x++;
+             y--;
+         }
+        System.out.println("true");
+
+
+
+
 
     }
 }
